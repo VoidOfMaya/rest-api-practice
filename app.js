3285b9943382b4effec45a496cadf6d12ab7221d
VoidOfMaya <mayaofthevoid@gmail.com>
2026-02-19 13:14:48 +0200

    implements relevant cors documentation

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import path from 'node:path';
 import { fileURLToPath } from "node:url";
 import 'dotenv/config'
-
+import cors from 'cors'
 import { indexRouter } from "./routes/indexRouter.js";
 import { userRouter } from "./routes/userRouter.js";
 import { messageRouter } from "./routes/messageRouter.js";
@@ -11,6 +11,16 @@ const app = express(()=>{
     console.log('booting server...')
 })
 
+// ===>ON CORS(CROSS ORIGIN REQUESTS)<======
+// Adds headers: Access-Control-Allow-Origin: * enables all COR
+//> app.use(cors())
+
+// Adds headers: Access-Control-Allow-Origin: * enables CORS for a single rout!
+
+//> app.get('/products/:id', cors(), function (req, res, next) {
+//>   res.json({msg: 'Hello'})
+//> })
+//==========================================
 //the following 2 lines are esm specific(dirname is only implicit in common.js)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,21 +1,30 @@
-import { Router } from "express";
-import { users, messages } from "../mockData.js";
+import { Router, text } from "express";
+import { messages } from "../mockData.js";
+import {v4 as uuidv4 } from 'uuid';
 const messageRouter = Router();
 
 messageRouter.get('/', (req, res) => {
-  return res.send(Object.values(messages));
+    return res.send(Object.values(messages));
 });
-
+messageRouter.get('/:id', (req, res) => {
+    return res.send(Object.values(messages[req.params.id]));
+});
 messageRouter.post('/', (req, res) => {
-  return res.send('POST HTTP method on user resource');
+        const id = uuidv4();
+    const message ={
+        id,
+        text: req.body.text,
+    };
+    messages[id]= message;
+    return res.send(message);
 });
 
 messageRouter.put('/:messageId', (req, res) => {
-  return res.send(Object.values(messages[req.params.messageId]));
+    return res.send(Object.values(messages[req.params.messageId]));
 });
 
 messageRouter.delete('/:messageId', (req, res) => {
-  return res.send(`DELETE HTTP method on user/${req.params.messageId} resource`);
+    return res.send(`DELETE HTTP method on user/${req.params.messageId} resource`);
 });
 
 export{
